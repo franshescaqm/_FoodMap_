a336e125df4c1e2ae3e3f95f1c6f5f633f40742a
franshescaqm <franshesca_94@hotmail.com>
2017-12-29 06:18:31 -0500

    adding images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function() {
+    setTimeout(function() {
+      window.location.href = 'views/filter.html';
+    }, 5000);
+  
+  });
---
+++ js/filter2.js
@@ -0,0 +1,336 @@
+var data = {
+    'VENTANILLA': {
+        'VItalian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'LA PUNTA': {
+        'LItalian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'BELLAVISTA': {
+        'BItalian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'LA PERLA': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'SAN MARTIN DE PORRES': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'LOS OLIVOS': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'COMAS': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'CERCADO DE LIMA': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'BREÑA': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'RIMAC': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'EL AGUSTINO': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'SAN MIGUEL': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
+          'opinion': 'good'
+        }
+    },
+    'PUEBLO LIBRE': {
+        'Italian Food': {
+          'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
+          'opinion': 'good'
+        },
+        'Japanese Food': {
+          'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
+          'opinion': 'good'
+        },
+        'Vegetarian Food' : {
+          'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
+          'opinion': 'good'
+        },
+        'Amazonian Food':{
+          'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
+          'opinion': 'good'
+        },
+        'Arabic Food':{
+          'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
+          'opinion': 'good'
+        },
+        'Cake Shop':{
+          'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Sho...
