77d2f5927e05717b9aa7702f36f1fdd2e0050482
franshescaqm <franshesca_94@hotmail.com>
2018-01-02 11:54:09 -0500

    correction

--- js/filter.js
+++ js/filter.js
@@ -1,31 +1,10 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   /*var pizza = $('#PIZZA');
   var calzone = $('#CALZONE');
   var noodles = $('#NOODLES');
-  var desserts = $('#DESSERTS');*/
-  $('#select').click('#PIZZA', function(){
+  var desserts = $('#DESSERTS');
+  $('#select').click('#PIZZA', function(){*/
 
-var textoFiltro = $(this).val();
 
-/*if(textoFiltro == 'todos' || textoFiltro == "")
-{
-  $('div.filtros div.hidden').fadeIn('slow').removeClass('hidden');
-}
-else
-{
-  $('.filtros div').each(function() {
-
-    if(!$(this).hasClass(textoFiltro))
-    {
-      $(this).fadeOut('normal').addClass('hidden');
-    }
-    else
-    {
-      $(this).fadeIn('slow').removeClass('hidden');
-    }
-  });
-}
-
-return false;*/
-  })
-});
+/*  })
+});*/
