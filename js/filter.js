6396424c46ed9767b2c630a9a3838918eab71335
franshescaqm <franshesca_94@hotmail.com>
2018-01-02 11:26:56 -0500

    adding changes/JS pending

--- js/filter.js
+++ js/filter.js
@@ -1,32 +1,31 @@
-$(document).ready(function(){
-    var $splash = $('#splash');  
-    var $platform = $('#platform'); 
-    var $map = $('#map');
-    var $searchRestaurant = $('#search-restaurant');
-    var $restaurantImages = $('div.restaurant-images');
-  
-    //creando array de objetos para representar data
-  
-    //Confirma que la dirección que se escojió funciona
-    $searchRestaurant.on('keyup', pickDistrict);
-  
-    //función PickDistrict para que jale los distritos
-    function pickDistrict(){
-      //debugger 
-      $.each(data, function(key, value){
-        if($searchRestaurant.val().toUpperCase() === key){
-          $('h3').text(key);
-          var restaurantList = data[key]; 
-          console.log(restaurantList); //{Italian Food: {…}, Japanese Food: {…}, Vegetarian Food: {…}, Amazonian Food: {…}, Arabic Food: {…}, …}
-          console.log(Object.keys(restaurantList).length); //6
-          console.log(Object.keys(restaurantList)); //(6) ["Italian Food", "Japanese Food", "Vegetarian Food", "Amazonian Food", "Arabic Food", "Cake Shop"]
-          console.log(Object.values(restaurantList)); //(6) [{…}, {…}, {…}, {…}, {…}, {…}]
-          console.log(Object.values(restaurantList)[0]); //{photo: "http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg", opinion: "good"}
-          for(var i = 0; i < Object.keys(restaurantList).length; i++){
-  
-            $restaurantImages.addClass('style-restaurant').eq(i).text(Object.keys(restaurantList)[i]);
-          }
-        };
-      });
-    };  
+$(document).ready(function() {
+  /*var pizza = $('#PIZZA');
+  var calzone = $('#CALZONE');
+  var noodles = $('#NOODLES');
+  var desserts = $('#DESSERTS');*/
+  $('#select').click('#PIZZA', function(){
+
+var textoFiltro = $(this).val();
+
+/*if(textoFiltro == 'todos' || textoFiltro == "")
+{
+  $('div.filtros div.hidden').fadeIn('slow').removeClass('hidden');
+}
+else
+{
+  $('.filtros div').each(function() {
+
+    if(!$(this).hasClass(textoFiltro))
+    {
+      $(this).fadeOut('normal').addClass('hidden');
+    }
+    else
+    {
+      $(this).fadeIn('slow').removeClass('hidden');
+    }
   });
+}
+
+return false;*/
+  })
+});
