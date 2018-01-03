c261da457734a223af19890855f1b95634b8c94c
franshescaqm <franshesca_94@hotmail.com>
2018-01-03 15:54:51 -0500

    adding pop-up informative modal for food images

--- js/filter.js
+++ js/filter.js
@@ -1,10 +1,11 @@
-/*$(document).ready(function() {
+
+$(document).ready(function() {
   /*var pizza = $('#PIZZA');
   var calzone = $('#CALZONE');
   var noodles = $('#NOODLES');
-  var desserts = $('#DESSERTS');
-  $('#select').click('#PIZZA', function(){*/
+  var desserts = $('#DESSERTS');*/
+  $('#select option').each(function(){
+    $(this).val()
 
-
-/*  })
-});*/
+  })
+});
